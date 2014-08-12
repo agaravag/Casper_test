5cb8227706f0d41410b12a595aafd12391ecd7fc
agaravag <agaravag@hotmail.com>
2014-08-11 21:13:12 -0700

    add casper

---
+++ test.js
@@ -0,0 +1,12 @@
+
+casper.test.begin('testing our REST API', 1, function suite(test) {
+
+casper.start('http://localhost:3000/', function() {
+    test.assertHttpStatus(200);
+  });
+
+casper.run(function() {
+  test.done();
+});
+
+});
